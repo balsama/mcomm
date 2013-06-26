8627868bd976313dc9f84a56887a95edc46f85f2
Adam Balsam <balsam.adam@gmail.com>
2013-06-26 13:55:39 -0400

    Added missing social media block

---
+++ templates/mcomm-sm.tpl.php
@@ -0,0 +1,9 @@
+<div class="clearfix">
+  <div class="gswu">Get Social With Us!</div>
+  <div class="sm slsm-1">
+  <a href="<?php print $commset['sm_links']['facebook']; ?>"></a>
+  </div>
+  <div class="sm slsm-2">
+    <a href="<?php print $commset['sm_links']['twitter']; ?>"></a>
+  </div>
+</div>
